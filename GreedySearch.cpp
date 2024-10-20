41a05a6fbedd21526d66f4563dbd27e6ce78d396
mariannastamati <sdi2000183@di.uoa.gr>
2024-10-20 20:07:54 +0300

    initializatios of greedy

--- GreedySearch.cpp
+++ GreedySearch.cpp
@@ -13,6 +13,15 @@ bool compare(int a, int b, const vector<float>& query, const vector<vector<float
     return EuclideanDistance(query, vectors[a]) > EuclideanDistance(query, vectors[b]);
 }
 
+// Greedy Search 
+vector<int> GreedySearch(const vector<vector<edge>>& graph, const vector<float>& query, const vector<vector<float>>& vectors, int start_node, int k, int L){
+
+
+    set<int> visited;                 // set for the nodes we have visited
+    vector<int> result_set;           // result set 
+    vector<int> search_list = {start_node};   // search list which we initialize with the start node 
+    
+    visited.insert(start_node);       
 
 
 
@@ -20,3 +29,4 @@ bool compare(int a, int b, const vector<float>& query, const vector<vector<float
 
 
 
+}
