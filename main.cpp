dcc8865dd075c60b10071fae2a06b08bc04c25b3
zodicx <andrew.jevtovic@gmail.com>
2024-10-21 20:49:47 +0300

    Changes so that the Prune Algorithm runs and prints of nodes and edges

--- main.cpp
+++ main.cpp
@@ -4,17 +4,39 @@
 
 #include "readfiles.h"
 #include "VamanaIndexing.h"
+#include "robustPrune.h"
+#include "printGraph.h"
 
 
-int main(){
-    
+int main() {
     // Reading input files
-    string filename = "../datasets/siftsmall/siftsmall_base.fvecs";
-    vector<vector<float>> vectors = read_fvecs(filename);
+    std::string filename = "../datasets/siftsmall/siftsmall_base.fvecs";
+    std::vector<std::vector<float>> vectors = read_fvecs(filename);
 
     // Vamana Indexing Algorithm (Creation of Vamana Graph)
-    vector<vector<edge>> VamanaGraph = VamanaIndexing(vectors, vectors.size(), 500);
+    std::vector<std::vector<edge>> VamanaGraph = VamanaIndexing(vectors, vectors.size(), 500);
+
+    // Print the size of the Vamana graph (before pruning)
+    std::cout << "Size of VamanaGraph (before pruning): " << VamanaGraph.size() << std::endl;
+    std::cout << "Total edges before pruning: " << CountTotalEdges(VamanaGraph) << std::endl;
+
+
+
+
+    // ROBUST PRUNE SECTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Prepare inputs for RobustPrune
+    int p = 0;  // The first node that the algorithm of Prune will start
+    std::vector<int> V(vectors.size());
+    std::iota(V.begin(), V.end(), 0);  // Fill V with all node indices
+    float a = 2.0f;  // Scaling factor
+    int R = 100;     // Regularity
+
+    // Call RobustPrune
+    RobustPrune(p, V, a, R, VamanaGraph);
+
+    // Print the size of the Vamana graph (after pruning)
+    std::cout << "Size of VamanaGraph (after pruning): " << VamanaGraph.size() << std::endl;
+    std::cout << "Total edges after pruning: " << CountTotalEdges(VamanaGraph) << std::endl;
 
     return 0;
-
 }
