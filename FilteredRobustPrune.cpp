6f974b8ab41cf49ef397613c2fe49ec01c0d8afd
zodicx <andrew.jevtovic@gmail.com>
2024-11-28 18:22:13 +0200

    .h files correction

--- FilteredRobustPrune.cpp
+++ FilteredRobustPrune.cpp
@@ -1,6 +1,4 @@
 #include "FilteredRobustPrune.h"
-#include "graph.h"
-#include "cleandata.h"
 
 vector <graph> FilteredRobustPrune(int currentPoint, vector<int>& visited_nodes, float a, int R, vector<graph>& G, const vector<vector<float>>& data)
 {
